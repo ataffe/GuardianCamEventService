7cb06b2c26856f69541475bf1cab46e9e9086552
ataffe <taffealexander@gmail.com>
2026-03-19 01:27:33 -0700

    Added rest endpoint to recieve an image and added blank client file.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, File, UploadFile, HTTPException
+import cv2
+import numpy as np
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool | None = None
-
-@app.get("/")
-async def root():
-    return {"Hello": "World"}
+@app.post("/predict", status_code=201)
+async def predict(file: UploadFile = File(...)):
+    content = await file.read()
+    nparr = np.frombuffer(content, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    if img is None:
+        raise HTTPException(status_code=400, detail="Could not decode the image.")
+    cv2.imwrite("test.jpg", img)
+    return {
+        "success": True,
+        "message": f"Image shape {img.shape} successfully uploaded",
+    }
